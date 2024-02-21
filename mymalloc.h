5655466fb6e2c45156092e0890463c0229aec72f
Ydpatel12 <155326501+Ydpatel12@users.noreply.github.com>
2024-02-20 19:42:15 -0500

    Create mymalloc.h

---
+++ mymalloc.h
@@ -0,0 +1,16 @@
+#ifndef MYMALLOC_H
+#define MYMALLOC_H
+
+#define malloc(s) mymalloc(s, __FILE__, __LINE__)
+#define free(p) myfree(p, __FILE__, __LINE__)
+
+void *mymalloc(size_t size, char *file, int line);
+void myfree(void *ptr, char *file, int line);
+
+
+void error(int type,char*file,int line);
+
+void newMetadata(int num);
+void coalesce();
+
+#endif
